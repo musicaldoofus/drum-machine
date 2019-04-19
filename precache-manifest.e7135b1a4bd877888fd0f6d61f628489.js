c8a0941c8bb315aee218b13590233f0c42dfcb90
musicaldoofus <burns.trainingdesign@gmail.com>
2019-04-19 11:49:29 -0700

    Updates

---
+++ precache-manifest.e7135b1a4bd877888fd0f6d61f628489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "d136437ac08d3d0f6f8f",
+    "url": "/drum-machine/static/js/main.44a3abc1.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/drum-machine/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "d136437ac08d3d0f6f8f",
+    "url": "/drum-machine/static/css/main.b8940836.chunk.css"
+  },
+  {
+    "revision": "c00b919ca61e6f79228758c54f245c33",
+    "url": "/drum-machine/index.html"
+  }
+];
