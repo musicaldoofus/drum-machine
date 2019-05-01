f181c7038bf04ff9f9e98f477990f663c85e5556
musicaldoofus <burns.trainingdesign@gmail.com>
2019-05-01 14:46:49 -0700

    added queue for writing >= 8 beats to memo; added Howler as a dependency to manage audio playback; improved play workflow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { Howl } from 'howler';
 import audioSrc from './audioSrc';
 import Pad from './Pad';
+import QueuePad from './QueuePad';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			activePad: null
+			activePad: null,
+			playQueue: [],
+			playing: false,
+			activeQueueIndex: 0,
+			tempo: 120 //BPM
 		};
+		this.sounds = audioSrc.map(s => new Howl({src: [s.wavSrc]}));
 		this.handlePadClick = this.handlePadClick.bind(this);
+		this.handleAddSound = this.handleAddSound.bind(this);
+		this.handlePlayQueue = this.handlePlayQueue.bind(this);
+		this.handleUpdateTempo = this.handleUpdateTempo.bind(this);
 	}
 	
 	componentDidMount() {
@@ -19,7 +29,19 @@ class App extends Component {
 		});
 	}
 	
+	playSound(keyCode) {
+		let ind = 0;
+		for (let i = 0; i < this.sounds.length; i++) {
+			if (audioSrc[i].keyCode === keyCode) ind = i;
+		}
+		this.sounds[ind].play();
+	}
+	
 	handlePadKeyCodePress(keyCode) {
+		this.playSound(keyCode);
+	}
+	
+	OLD_handlePadKeyCodePress(keyCode) {
 		const audioKeyCode = audioSrc.filter(s => s.keyCode === keyCode);
 		if (audioKeyCode.length === 0) return;
 		document.getElementById(audioKeyCode[0].srcDescription).click();
@@ -27,6 +49,36 @@ class App extends Component {
 	
 	handlePadClick(keyCode) {
 		this.setState({activePad: keyCode});
+		!this.state.playing ? this.handleAddSound(keyCode) : console.log('playing');
+	}
+	
+	handleAddSound(keyCode) {
+		if (this.state.playQueue.length >= 8) return;
+		const currQueue = this.state.playQueue.slice();
+		const playQueue = currQueue.concat(keyCode);
+		this.setState({playQueue});
+	}
+	
+	handlePlayQueue() {
+		this.setState({playing: true});
+		const tempoMilliseconds = (1/(this.state.tempo/60)) * 1000; //convert from BPM to milliseconds
+		const that = this;
+		const playFromQueue = () => {
+			this.playSound(this.state.playQueue[this.state.activeQueueIndex]);
+			if (this.state.activeQueueIndex < (this.state.playQueue.length - 1)) {
+				that.setState({activeQueueIndex: that.state.activeQueueIndex + 1});
+			}
+			else {
+				window.clearInterval(queueSounds);
+				this.setState({playing: false, activeQueueIndex: 0});
+			}
+		};
+		const queueSounds = window.setInterval(playFromQueue, tempoMilliseconds);
+	}
+	
+	handleUpdateTempo(a) {
+		const tempo = this.state.tempo + a;
+		this.setState({tempo});
 	}
 	
 	render() {
@@ -36,17 +88,28 @@ class App extends Component {
 				onClick={this.handlePadClick}
 				{...s}
 			/>
-		
 		));
+		const queue = this.state.playQueue.map((k, i) => <QueuePad key={i} keyCode={k}/>);
 		return (
-			<div id="drum-machine" className="drum-machine">
-				<div id="display" className="display">
-					<div>
-						<p>{this.state.activePad ? audioSrc.filter(s => s.keyCode === this.state.activePad)[0].srcDescription : null}</p>
+			<div className="app-wrapper">
+				<div id="drum-machine" className="drum-machine">
+					<div id="display" className="display">
+						<div>
+							<p>{this.state.activePad ? audioSrc.filter(s => s.keyCode === this.state.activePad)[0].srcDescription : null}</p>
+						</div>
+						<div className="tempo-wrapper">
+							<button className="tempo-btn" onClick={() => this.handleUpdateTempo(5)}>+</button>
+							<button className="tempo-btn" onClick={() => this.handleUpdateTempo(-5)}>-</button>
+							<p>{this.state.tempo}</p>
+						</div>
+					</div>
+					<div className="pad-wrapper">
+						{pads}
 					</div>
 				</div>
-				<div className="pad-wrapper">
-					{pads}
+				<div className="queue-wrapper">
+					<button onClick={this.handlePlayQueue}>Play</button>
+					{queue}
 				</div>
 			</div>
 		);
