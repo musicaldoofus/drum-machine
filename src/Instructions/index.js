ddcb7323909c72bc8ae5b564c7cc52661ee09255
musicaldoofus <burns.trainingdesign@gmail.com>
2019-08-21 15:09:04 -0700

    Basic Bounce animations on instructions

--- src/Instructions/index.js
+++ src/Instructions/index.js
@@ -7,23 +7,33 @@ class Instructions extends Component {
 	constructor() {
 		super();
 		this.state = {
-			isDrumExpanded: false,
-			isPianoExpanded: false
+			expanded: null
 		};
 		this.handleToggleExpand = this.handleToggleExpand.bind(this);
+		this.handleWindowResize = this.handleWindowResize.bind(this);
+	}
+	
+	componentDidMount() {
+		window.addEventListener('resize', this.handleWindowResize);
 	}
 	
 	handleToggleExpand(type) {
-		this.setState({[type]: !this.state[type]});
+		console.log(type);
+		if (this.state.expanded === type) type = null;
+		this.setState({expanded: type});
+	}
+	
+	handleWindowResize() {
+		this.setState({expanded: null});
 	}
 	
 	render() {
 		return (
 			<Fragment>
-				<div className={`ui-instructions drum${this.state.isDrumExpanded ? ' active' : ''}`} onClick={() => this.handleToggleExpand('isDrumExpanded')}>
+				<div className={`ui-instructions drum${this.state.expanded === 'drum' ? ' active' : ''}`} onClick={() => this.handleToggleExpand('drum')}>
 					<img src={uiInstructionsDrum} alt="See which keys to use to play drum beats"/>
 				</div>
-				<div className={`ui-instructions piano${this.state.isPianoExpanded ? ' active' : ''}`} onClick={() => this.handleToggleExpand('isPianoExpanded')}>
+				<div className={`ui-instructions piano${this.state.expanded === 'piano' ? ' active' : ''}`} onClick={() => this.handleToggleExpand('piano')}>
 					<img src={uiInstructionsPiano} alt="See which keys to use to play notes on the piano"/>
 				</div>
 			</Fragment>
